81bca555477ad90571adc8e6b48c347ad80359cb
Maeng <maeng2418@naver.com>
2021-10-25 08:05:56 +0900

    [chore] 타이포그래피 스토리북 내용 수정

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -9,11 +9,17 @@ export default {
 
 export const Template: Story = () => (
   <Typography>
-    <Typography.Title>Introduction</Typography.Title>
+    <Typography.Title level={3}>소개</Typography.Title>
     <Typography.Paragraph>
-      In the process of internal desktop applications development, many different design specs and
-      implementations would be involved, which might cause designers and developers difficulties and
-      duplication and reduce the efficiency of development.
+      내부 애플리케이션 개발 과정에서 다양한 설계 사양과 구현으로 인해 디자이너와 개발자간의 소통의
+      어려움이 발생하게 되고 개발 효율성을 떨어뜨릴 수 있다.
+    </Typography.Paragraph>
+    <Typography.Paragraph>
+      <Typography.Text>디자인 시스템 Maeng Design은 내부 프로젝트를 위한 </Typography.Text>
+      <Typography.Text strong>
+        사용자 인터페이스 사양을 통일하고, 설계 차이와 구현에 필요한 불필요한 비용을 낮추고, 설계 및
+        프론트엔드 개발 자원을 지원하는 것을 목표로 하는 Maeng에 의해 개발되었습니다.
+      </Typography.Text>
     </Typography.Paragraph>
   </Typography>
 );
