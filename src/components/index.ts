513b994c4930789c43f74555894cd700f6570675
Maeng <maeng2418@naver.com>
2021-11-22 10:59:46 +0900

    [feat] Tag 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
 export { default as Divider } from './Divider';
 export { CaretDownOutlined, CaretLeftOutlined, CaretRightOutlined, CaretUpOutlined } from './Icons';
+export { default as Tag } from './Tag';
 export { default as Typography } from './Typography';
