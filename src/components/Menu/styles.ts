56e11808cd02641d24a5064fad06b6d0c9dfa21c
Maeng <maeng2418@naver.com>
2021-12-03 14:35:10 +0900

    [chore] Menu 스타일 분리

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -7,11 +7,12 @@ const createStyle =
     // color
     const primaryColor = getColor(theme, color);
 
-    // default
-    const defaultStyle = css`
+    const navStyle = css`
       border-bottom: 1px solid ${getColor(theme, 'gray4')};
       box-shadow: none;
+    `;
 
+    const mainMenuStyle = css`
       & ul.main-menu {
         list-style: none;
         height: 46px;
@@ -20,52 +21,7 @@ const createStyle =
       }
     `;
 
-    // itemGroupStyle
-    const itemGroupStyle = css`
-      & ul.sub-menu {
-        min-width: 100%;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        border-radius: 4px;
-        position: absolute;
-        top: 50px;
-        background-color: ${getColor(theme, 'gray1')};
-        transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-        box-shadow: 0 3px 6px -4px rgb(0 0 0 / 12%), 0 6px 16px 0 rgb(0 0 0 / 8%),
-          0 9px 28px 8px rgb(0 0 0 / 5%);
-        opacity: 0;
-
-        & li {
-          float: none;
-          padding: 0;
-          margin: 0;
-
-          & a {
-            display: flex;
-            font-size: 14px;
-            padding: 4px 15px;
-            color: ${getColor(theme, 'gray13')};
-            text-decoration: none;
-            align-items: center;
-            white-space: nowrap;
-
-            &:hover {
-              color: ${primaryColor};
-              background: ${getColor(theme, 'gray1')};
-            }
-          }
-
-          &.selected a {
-            color: ${primaryColor};
-            background: ${getColor(theme, 'gray1')};
-          }
-        }
-      }
-    `;
-
-    // itemStyle
-    const itemStyle = css`
+    const mainMenuItemStyle = css`
       & ul.main-menu {
         & > li {
           float: left; // 메뉴를 왼쪽부터 표시
@@ -95,131 +51,163 @@ const createStyle =
 
           &.selected {
             border-bottom: 2px solid ${primaryColor};
-            a {
+            & > a {
               color: ${primaryColor};
               background: ${getColor(theme, 'gray1')};
             }
           }
-
-          & ul.sub-menu {
-            & li {
-              float: none;
-              padding: 0;
-              margin: 0;
-
-              & a {
-                display: flex;
-                font-size: 14px;
-                padding: 4px 15px;
-                color: ${getColor(theme, 'gray13')};
-                text-decoration: none;
-                align-items: center;
-                white-space: nowrap;
-
-                &:hover {
-                  color: ${primaryColor};
-                  background: ${getColor(theme, 'gray1')};
-                }
-              }
-
-              &.selected a {
-                color: ${primaryColor};
-                background: ${getColor(theme, 'gray1')};
-              }
-            }
-          }
         }
       }
     `;
 
-    const verticalStyle = css`
+    const subMenuStyle = css`
+      & ul.sub-menu {
+        min-width: 100%;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        border-radius: 4px;
+        position: absolute;
+        top: 50px;
+        background-color: ${getColor(theme, 'gray1')};
+        transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+        box-shadow: 0 3px 6px -4px rgb(0 0 0 / 12%), 0 6px 16px 0 rgb(0 0 0 / 8%),
+          0 9px 28px 8px rgb(0 0 0 / 5%);
+        opacity: 0;
+      }
+    `;
+
+    const subMenuItemStyle = css`
+      & ul.sub-menu li {
+        float: none;
+        padding: 0;
+        margin: 0;
+
+        & a {
+          display: flex;
+          font-size: 14px;
+          padding: 4px 15px;
+          color: ${getColor(theme, 'gray13')};
+          text-decoration: none;
+          align-items: center;
+          white-space: nowrap;
+
+          &:hover {
+            color: ${primaryColor};
+            background: ${getColor(theme, 'gray1')};
+          }
+        }
+
+        &.selected a {
+          color: ${primaryColor};
+          background: ${getColor(theme, 'gray1')};
+        }
+      }
+    `;
+
+    const verticalNavStyle = css`
       border-right: 1px solid ${getColor(theme, 'gray4')};
       width: 256px;
+    `;
 
+    const verticalMainMenuStyle = css`
       & ul.main-menu {
         margin: 0;
         padding: 0;
         list-style: none;
+      }
+    `;
 
-        & li {
-          padding: 0;
-          margin: 0;
+    const verticalMainMenuItemStyle = css`
+      & ul.main-menu li {
+        padding: 0;
+        margin: 0;
 
-          a {
-            display: flex;
-            height: 40px;
-            line-height: 40px;
-            font-size: 14px;
-            padding: 4px 16px;
-            color: ${getColor(theme, 'gray13')};
-            text-decoration: none;
-            align-items: center;
-            white-space: nowrap;
-            justify-content: space-between;
+        a {
+          display: flex;
+          height: 40px;
+          line-height: 40px;
+          font-size: 14px;
+          padding: 4px 16px;
+          color: ${getColor(theme, 'gray13')};
+          text-decoration: none;
+          align-items: center;
+          white-space: nowrap;
+          justify-content: space-between;
 
-            & svg {
-              margin-right: 8px;
-            }
+          & svg {
+            margin-right: 8px;
           }
-          &:hover a {
+        }
+        &:hover > a {
+          color: ${primaryColor};
+
+          & svg {
+            fill: ${primaryColor};
+          }
+        }
+
+        &.selected {
+          &.item {
+            border-right: 3px solid ${primaryColor};
+            background: ${getColor(theme, 'blue1')};
+          }
+          & > a {
             color: ${primaryColor};
-
-            & svg {
-              fill: ${primaryColor};
-            }
           }
-
-          &.selected {
-            &.item {
-              border-right: 3px solid ${primaryColor};
-              background: ${getColor(theme, 'blue1')};
-            }
-            & a {
-              color: ${primaryColor};
-            }
-            & ul.sub-menu {
-              display: block;
-            }
-          }
-
           & ul.sub-menu {
-            margin: 0;
-            padding: 0;
-            list-style: none;
-            background: ${getColor(theme, 'gray2')};
-            display: none;
-
-            & li {
-              padding: 0;
-              margin: 0;
-
-              a {
-                display: flex;
-                height: 40px;
-                line-height: 40px;
-                font-size: 14px;
-                padding: 4px 16px 4px 48px;
-                color: ${getColor(theme, 'gray13')};
-                text-decoration: none;
-                align-items: center;
-                white-space: nowrap;
-              }
-
-              &:hover a {
-                color: ${primaryColor};
-              }
-
-              &.selected a {
-                color: ${primaryColor};
-              }
-            }
+            display: block;
           }
         }
       }
     `;
 
-    if (mode === 'vertical') return [verticalStyle];
-    return [defaultStyle, itemGroupStyle, itemStyle];
+    const verticalSubMenuStyle = css`
+      & ul.sub-menu {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        background: ${getColor(theme, 'gray2')};
+        display: none;
+      }
+    `;
+
+    const verticalSubMenuItemStyle = css`
+      & ul.sub-menu li {
+        padding: 0;
+        margin: 0;
+
+        a {
+          display: flex;
+          height: 40px;
+          line-height: 40px;
+          font-size: 14px;
+          padding: 4px 16px 4px 48px;
+          color: ${getColor(theme, 'gray13')};
+          text-decoration: none;
+          align-items: center;
+          white-space: nowrap;
+        }
+
+        &:hover a {
+          color: ${primaryColor};
+        }
+
+        &.selected a {
+          color: ${primaryColor};
+        }
+      }
+    `;
+
+    if (mode === 'vertical')
+      return [
+        verticalNavStyle,
+        verticalMainMenuStyle,
+        verticalMainMenuItemStyle,
+        verticalSubMenuStyle,
+        verticalSubMenuItemStyle,
+      ];
+    return [navStyle, mainMenuStyle, mainMenuItemStyle, subMenuStyle, subMenuItemStyle];
   };
 
 export default createStyle;
