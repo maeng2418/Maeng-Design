1fe558a1e3725c98adc3ea8f77775c826ec811ee
Maeng <maeng2418@naver.com>
2021-12-23 17:22:02 +0900

    [chore] REGEX 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './components';
 export * from './hooks';
 export * from './styles';
+export { default } from './utils/regex';
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,15 @@
-export const STRING_REGEX = /[^a-zA-Z]/g;
+/** color?.replace(NOT_STRING_REGEX, '')}${SUB_COLOR_IDX} */
+const NOT_STRING_REGEX = /[^a-zA-Z]/g;
+
+const NOT_NUM_REGEX = /[^0-9]/;
+
+/** String.value.replace(NOT_NUM_REGEX, '').replace(PHONE_NUMBER_REGEX, `$1-$2-$3`); */
+const PHONE_NUMBER_REGEX = /^(\d{2,3})(\d{3,4})(\d{4})$/;
+
+const REGEX = {
+  NOT_STRING_REGEX,
+  NOT_NUM_REGEX,
+  PHONE_NUMBER_REGEX,
+};
+
+export default REGEX;
