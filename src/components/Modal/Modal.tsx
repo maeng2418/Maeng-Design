15867b95e7419b043dc8a481b7e2b69be9520f1c
Maeng <maeng2418@naver.com>
2022-01-06 22:39:12 +0900

    [style] 모달 스타일 수정 - z-index 수치 조정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import { css, Global, Interpolation, Theme, ThemeMode } from '@emotion/react';
+import { Global } from '@emotion/react';
 import React, { MouseEvent, ReactNode, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { Button, CloseOutlined } from '..';
-import { DarkColorType, getColor, LightColorType } from '../../styles/colors';
-import sizeTranslator from '../../utils/sizeTranslator';
+import { DarkColorType, LightColorType } from '../../styles/colors';
 import { ButtonProps } from '../Button/Button';
+import createStyle, { globalStyle } from './styles';
 
 export interface ModalProps {
   color?: LightColorType | DarkColorType;
@@ -72,7 +72,7 @@ const Modal: React.FC<ModalProps> = ({
     ? createPortal(
         <>
           <Global styles={globalStyle} />
-          <article css={modalStyle(width, color)}>
+          <article css={createStyle(width, color)}>
             <section className="modal">
               {(title || closeIcon || onClose) && (
                 <header className="modal-header">
@@ -108,75 +108,5 @@ const Modal: React.FC<ModalProps> = ({
       )
     : null;
 };
-
-const globalStyle = css`
-  body {
-    overflow: hidden !important;
-  }
-`;
-
-const modalStyle =
-  (width: ModalProps['width'] = '400px', color: ModalProps['color']) =>
-  (theme: Theme = { mode: ThemeMode.LIGHT }): Interpolation<Theme> => {
-    return css`
-      position: fixed;
-      top: 0;
-      width: 100vw;
-      height: 100vh;
-
-      .modal {
-        position: relative;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        display: flex;
-        flex-direction: column;
-        background: ${getColor(theme, 'gray1')};
-        border-radius: 2px;
-        width: ${sizeTranslator(width)};
-        max-width: calc(100vw - 32px);
-        z-index: 1;
-        .modal-header {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding: 16px 24px;
-          border-bottom: 1px solid ${getColor(theme, 'gray4')};
-          h3 {
-            padding: 0;
-            margin: 0;
-          }
-          .close-icon {
-            cursor: pointer;
-            fill: ${getColor(theme, 'gray7')};
-          }
-        }
-        .modal-content {
-          padding: 24px;
-        }
-        .modal-footer {
-          padding: 10px 16px;
-          text-align: right;
-          background: 0 0;
-          border-top: 1px solid ${getColor(theme, 'gray4')};
-          border-radius: 0 0 2px 2px;
-
-          & button {
-            margin-left: 8px;
-          }
-        }
-      }
-      .background {
-        cursor: pointer;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        background-color: ${getColor(theme, 'gray13')};
-        opacity: 0.3;
-        z-index: -1;
-      }
-    `;
-  };
 
 export default Modal;
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,17 +1,76 @@
 import { css, Interpolation, Theme } from '@emotion/react';
 import { getColor, ThemeMode } from '../../styles';
+import sizeTranslator from '../../utils/sizeTranslator';
 import { ModalProps } from './Modal';
 
 const createStyle =
-  (color?: ModalProps['color']) =>
+  (width: ModalProps['width'] = '400px', color: ModalProps['color']) =>
   (theme: Theme = { mode: ThemeMode.LIGHT }): Interpolation<Theme> => {
-    // color
-    const primaryColor = getColor(theme, color);
+    return css`
+      position: fixed;
+      top: 0;
+      width: 100vw;
+      height: 100vh;
+      z-index: 9999;
 
-    // default
-    const defaultStyle = css``;
+      .modal {
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-direction: column;
+        background: ${getColor(theme, 'gray1')};
+        border-radius: 2px;
+        width: ${sizeTranslator(width)};
+        max-width: calc(100vw - 32px);
+        z-index: 1;
+        .modal-header {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 16px 24px;
+          border-bottom: 1px solid ${getColor(theme, 'gray4')};
+          h3 {
+            padding: 0;
+            margin: 0;
+          }
+          .close-icon {
+            cursor: pointer;
+            fill: ${getColor(theme, 'gray7')};
+          }
+        }
+        .modal-content {
+          padding: 24px;
+        }
+        .modal-footer {
+          padding: 10px 16px;
+          text-align: right;
+          background: 0 0;
+          border-top: 1px solid ${getColor(theme, 'gray4')};
+          border-radius: 0 0 2px 2px;
 
-    return [defaultStyle];
+          & button {
+            margin-left: 8px;
+          }
+        }
+      }
+      .background {
+        cursor: pointer;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: ${getColor(theme, 'gray13')};
+        opacity: 0.3;
+        z-index: -1;
+      }
+    `;
   };
 
+export const globalStyle = css`
+  body {
+    overflow: hidden !important;
+  }
+`;
 export default createStyle;
